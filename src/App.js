34069192bc0afdc211b23c48568b1d4f4889c20d
Bao Quoc <baoquocdhyd@gmail.com>
2022-05-06 22:03:00 +0700

    deploy heroku

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss'
 import Header from './components/Header.js'
 import TableUsers from './components/TableUsers.js'
+import Container from 'react-bootstrap/Container'
+
 function App() {
   return (
     <div className="app-container">
       <h1>Hello word</h1>
       <Header />
-      <TableUsers />
+      <Container>
+        <TableUsers />
+      </Container>
     </div>
   )
 }
--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,5 +1,20 @@
 import Table from 'react-bootstrap/Table'
+import axios from 'axios'
+import moment from 'moment'
+import dateFormat from 'dateformat'
+
+import { useState, useLayoutEffect } from 'react'
+
 const TableUsers = (props) => {
+  const [a, setA] = useState([])
+
+  const Fi = async () => {
+    const A = await axios.get('https://danhsachdiennao.herokuapp.com/api/get')
+    setA(A.data)
+  }
+  useLayoutEffect(() => {
+    Fi()
+  }, [])
   return (
     <>
       <Table striped bordered hover>
@@ -12,23 +27,29 @@ const TableUsers = (props) => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {a.map((a, b) => {
+            return (
+              <tr key={b}>
+                <td style={{ textAlign: 'center', backgroundColor: 'pink' }}>{a.id}</td>
+                <td style={{ textTransform: 'capitalize' }}>
+                  <div style={{ color: 'red', fontSize: '14px' }}>
+                    {' '}
+                    {a.ho} {a.ten}
+                  </div>
+                  <div> - SNV: {a.sohoso}</div>
+                  <div>- SĐT: {a.sodienthoai}</div>
+                </td>
+                <td>
+                  <div> {a.gioitinh === true ? 'Nam' : 'Nữ'}</div>
+                  <div> {dateFormat(a.namsinh, 'yyyy')}</div>
+                </td>
+                <td style={{ textAlign: 'left', backgroundColor: 'rgb(195, 255, 217)' }}>
+                  {moment(a.ngayhendo).format('HH:mmA ddd')} <br />
+                  {moment(a.ngayhendo).format('DD-MM-YYYY ')}
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </Table>
     </>
